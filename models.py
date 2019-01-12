1be747219e0289ebe9632e1b8c9a2cc31da4273c
Waseem Yusuf <waseem.yusuf.hossain@gmail.com>
2019-01-12 18:31:14 -0500

    Use Flask SQLAlchemy to define basic shema

---
+++ models.py
@@ -0,0 +1,11 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Products(db.Model):
+    __tablename__ = 'products'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    inventory_count = db.Column(db.Integer, nullable=False)
