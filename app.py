b5dc2ffe53b0d7bd08fd4fcdcc9ef4194c52c54a
Waseem Yusuf <waseem.yusuf.hossain@gmail.com>
2019-01-12 21:26:01 -0500

    Create endpoint for fetching all products

--- app.py
+++ app.py
@@ -2,18 +2,37 @@ import os
 from models import *
 from flask import Flask, jsonify, request
 
+# Configure database
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
 
-@app.route('/api/<int:product_id>', methods=['GET'])
-def fruit_api(product_id):
-    """Returns the price of a product"""
+@app.route('/api/products', methods=['GET'])
+def fetch_all():
 
+    isAvailable = request.args.get('available')
+    if isAvailable:
+        products = Product.query.filter(Product.inventory_count > 0).all()
+    else:
+        products = Product.query.all()
+
+    response = {}
+
+    for product in products:
+        response['inventory_count'] = product.inventory_count
+        response['price'] = product.price
+        response['title'] = product.title
+        response['id'] = product.id
+
+    return jsonify(response)
+
+
+@app.route('/api/products/<int:product_id>', methods=['GET'])
+def fetch_one(product_id):
     # Check if product is valid
-    product = Products.query.get(product_id)
+    product = Product.query.get(product_id)
     if product is None:
         return jsonify({'error': 'Invalid product'}), 422
 
