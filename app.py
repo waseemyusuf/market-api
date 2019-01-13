ffe9e4dc8971ea2a0b8f47b3d38ec35e7f92bab4
Waseem Yusuf <waseem.yusuf.hossain@gmail.com>
2019-01-12 20:34:01 -0500

    Connect Flask app to the database

--- app.py
+++ app.py
@@ -1,26 +1,24 @@
-from flask import Flask, jsonify
+import os
+from models import *
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
 
-db = {
-    'apple': 12.00,
-    'mango': 01.99,
-    'banana': 24.78
-}
 
-@app.route('/api/<string:product>', methods=['GET'])
-def fruit_api(product):
+@app.route('/api/<int:product_id>', methods=['GET'])
+def fruit_api(product_id):
     """Returns the price of a product"""
 
     # Check if product is valid
-    if product not in db:
+    product = Products.query.get(product_id)
+    if product is None:
         return jsonify({'error': 'Invalid product'}), 422
 
     # Get the price
-    price = db[product]
+    price = product.price
     return jsonify({
         'price': price
     })
-
-if __name__ == '__main__':
-    app.run()
